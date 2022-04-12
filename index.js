437a86dffe23ff5eab0dec432f4edc79be76934d
matthew leary <matt-leary@live.co.uk>
2022-04-12 22:07:00 +0100

    commit tree 50c5b64bbce27d7478e700f15d62166d7bea6b5f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats = [ "Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+function appendCat(array) {
+    appendCat = [
+        ...cats.slice(),
+    "Broom"
+    ]
+    return appendCat;
+}
+function prependCat(name) {
+    prependCat = [
+        "Arnold",
+        ...cats.slice(),
+    ]
+    return prependCat;
+}
+function removeLastCat(name) {
+    removeLastCat = [
+        ...cats.slice(0, 2)
+    ]
+    return removeLastCat;
+}
+function removeFirstCat(name) {
+    removeFirstCat = [
+    ...cats.slice(1)
+]
+return removeFirstCat;
+}
